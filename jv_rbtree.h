f3941cb11804e2f47b93e53ae15fa0d873ebc0ba
Zane Chiang <jxvaer@gmail.com>
2018-03-17 10:00:25 +0800

    add jv_core.h

--- jv_rbtree.h
+++ jv_rbtree.h
@@ -1,24 +1,7 @@
 #ifndef _JV_RBTREE_H_INCLUDE_
 #define _JV_RBTREE_H_INCLUDE_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define JV_OK 0
-#define JV_ERROR -1
-
-#define jv_memzero(buf, n) (void *) memset(buf, 0, n)
-
-#ifndef intptr_t
-#define intptr_t long
-#define uintptr_t unsigned long
-#endif
-
-typedef intptr_t jv_int_t;
-typedef uintptr_t jv_uint_t;
-
-typedef unsigned char u_char;
+#include <jv_core.h>
 
 #define JV_RBTREE_BLACK 1
 #define JV_RBTREE_RED 0
